86736fc1c82a2f38d7f4bd7ff71b69c97c4b8ddd
Nazgolnvr <nazgolnvr93@gmail.com>
2023-01-06 16:32:57 +0330

    update java

---
+++ script.js
@@ -0,0 +1,5 @@
+<form>
+  <Input type="email" placeholder="Enter your email"></Input>
+  <Input type="password" placeholder="Enter your password"></Input>
+  <Input type="submit" value="Login"></Input>
+</form>;
